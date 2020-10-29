d5213b47f627b2f40e6d54030fcad8a1d7fdf818
John <John@PECET>
2020-10-29 20:52:13 +0100

    project new api

--- TransactionService/src/main/java/com/example/TransactionService/controller/TransactionController.java
+++ TransactionService/src/main/java/com/example/TransactionService/controller/TransactionController.java
@@ -2,13 +2,62 @@ package com.example.TransactionService.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/transactions")
+@RequestMapping
 public class TransactionController {
 
-    @GetMapping("/{username}")
-    public String getTransactions(@PathVariable String username) {
-        return "Welcome " + username;
+    // get assets || products || trades
+    @GetMapping("/assets")
+    public List<Asset> getAssets() {
+        return new List<Asset>();
+    }
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        return new List<Product>();
+    }
+
+    @GetMapping("/trades")
+    public List<Trade> getTrades() {
+        return new List<Trade>();
+    }
+
+    // add new asset
+    @PostMapping("/assets")
+    public String postAsset() {
+        return "";
+    }
+
+    // get asset by id
+    @GetMapping("/assets/{id}")
+    public Asset getAsset(@PathVariable String id) {
+        // TODO
+    }
+
+    // update existing asset
+    @PutMapping("/assets/{id}")
+    public String putAsset(@PathVariable String id) {
+        return "";
+    }
+
+    // get product by id
+    @GetMapping("/products/{id}")
+    public Asset getProduct(@PathVariable String id) {
+        // TODO
+    }
+
+    // update product by id
+    @PutMapping("/products/{id}")
+    public Asset putProduct(@PathVariable String id) {
+        // TODO
+    }
+
+    // search data by prhase
+    @GetMapping("/search/{phrase}")
+    public List<AbstractAsset> searchAsset(@PathVariable String phrase) {
+        // TODO
     }
 
 }
